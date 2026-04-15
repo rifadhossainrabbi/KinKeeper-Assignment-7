0e128f8aef37c3820507237b7a22561f2f2b4a93
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-04-15 15:09:53 +0600

    Details section responsive

--- src/pages/detailPage/DetailPage.jsx
+++ src/pages/detailPage/DetailPage.jsx
@@ -15,8 +15,6 @@ const DetailPage = () => {
   const { id } = useParams();
   const { friends, loading } = useFriends();
   const expectedId = friends.find((friend) => String(friend.id) == id);
-  // console.log(friends, loading);
-  // console.log(expectedId, 'expectedId');
 
   if (loading) {
     return (
@@ -25,13 +23,13 @@ const DetailPage = () => {
       </div>
     );
   }
+
   const today = new Date();
   const due = new Date(expectedId.next_due_date);
 
   const diffTime = today - due;
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-  // Formating data to human Readable
   const formatted = due.toDateString();
 
   const handleFriendsData = (type) => {
@@ -40,31 +38,32 @@ const DetailPage = () => {
       actionType: type,
     };
     const friendsCardIsExists = friendsCard.find(
-      (data) =>
-        data.id === expectedId.id && data.actionType === type,
+      (data) => data.id === expectedId.id && data.actionType === type,
     );
     if (!friendsCardIsExists) {
       setFriendsCard([...friendsCard, { ...expectedId, ...newExpectedData }]);
       toast.success(`Successfully ${type} ${expectedId.name}!`);
     } else {
-      toast.error(`Already added ${expectedId.name} to ${type}`)
+      toast.error(`Already added ${expectedId.name} to ${type}`);
     }
   };
 
   return (
     <div className="bg-[#f8fafc] py-12">
-      <div className="max-w-8/12 mx-auto grid gap-6 grid-cols-12">
-        {/* left side section */}
-        <div className="my-10 col-span-4">
+      <div className="max-w-11/12 md:max-w-10/12 lg:max-w-8/12 mx-auto grid gap-6 grid-cols-1 md:grid-cols-12 items-stretch">
+        {/* left section */}
+        <div className="my-10 md:col-span-4 lg:col-span-4 flex flex-col h-full">
+          {/* left section friend card */}
           <div className="bg-white shadow-xl p-6 rounded-md">
             <img
               src={expectedId.picture}
-              className="w-26 h-26 rounded-full mx-auto mb-2"
+              className="w-24 h-24 rounded-full mx-auto mb-2"
               alt={expectedId.name}
             />
             <p className="text-center font-semibold text-2xl">
               {expectedId.name}
             </p>
+
             <div className="flex justify-center mb-3">
               <p
                 className={`text-center ${
@@ -76,80 +75,89 @@ const DetailPage = () => {
                         ? 'bg-[#244d3f] px-3 py-1 mt-3 text-white rounded-full inline-block'
                         : ''
                 }`}>
-                <span className="justify-center mx-auto">
-                  {expectedId.status}
-                </span>
+                {expectedId.status}
               </p>
             </div>
-            <div className="flex gap-3 justify-center items-center">
+
+            <div className="flex flex-wrap gap-2 justify-center items-center">
               {expectedId.tags.map((tag, ind) => (
                 <p className="bg-[#cbfadb] px-3 py-1 rounded-full" key={ind}>
                   {tag}
                 </p>
               ))}
             </div>
+
             <p className="text-center font-semibold my-2.5">
               <em>
                 <q>{expectedId.bio}</q>
               </em>
             </p>
-            <p className="text-center">Prefered:{expectedId.email}</p>
-          </div>
-          <div className="bg-white shadow-xl mt-5 rounded-md">
-            <p className="flex items-center gap-1 py-2 justify-center text-center font-semibold">
-              <RiNotificationSnoozeLine />
-              Snooz {diffDays} days
+
+            <p className="text-center text-sm break-all px-2">
+              Prefered:{expectedId.email}
             </p>
           </div>
-          <div className="bg-white shadow-xl mt-3 py-2 rounded-md">
-            <p className="flex items-center gap-1 justify-center text-center font-semibold">
-              <PiArchive />
-              Archive
-            </p>
-          </div>
-          <div className="bg-white shadow-xl mt-3 rounded-md">
-            <p className="flex items-center gap-1 py-2 justify-center text-center text-red-500 font-semibold">
-              <RiDeleteBinLine color="red" />
-              Delete
-            </p>
+
+          {/* left section card action buttons */}
+          <div>
+            <div className="bg-white shadow-xl mt-5 rounded-md">
+              <p className="flex items-center gap-1 py-2 justify-center font-semibold">
+                <RiNotificationSnoozeLine />
+                Snooz {diffDays} days
+              </p>
+            </div>
+
+            <div className="bg-white shadow-xl mt-3 py-2 rounded-md">
+              <p className="flex items-center gap-1 justify-center font-semibold">
+                <PiArchive />
+                Archive
+              </p>
+            </div>
+
+            <div className="bg-white shadow-xl mt-3 rounded-md">
+              <p className="flex items-center gap-1 py-2 justify-center text-red-500 font-semibold">
+                <RiDeleteBinLine color="red" />
+                Delete
+              </p>
+            </div>
           </div>
         </div>
 
-        {/* Rignt side section */}
-        <div className="my-10 col-span-8 space-y-6">
-          <div className="grid grid-cols-3 gap-6">
+        {/* right section */}
+        <div className="my-10 md:col-span-8 lg:col-span-8 space-y-6 flex flex-col h-full">
+          {/* right section card activitise */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
             <div className="px-5 py-7 shadow bg-white rounded-md text-center">
-              <h1
-                className="text-2xl
-             font-semibold text-[#244D3F]">
+              <h1 className="text-2xl font-semibold text-[#244D3F]">
                 {expectedId.days_since_contact}
               </h1>
               <p className="text-[#64748B]">Days Since Contact</p>
             </div>
+
             <div className="px-5 py-7 shadow bg-white rounded-md text-center">
-              <h1
-                className="text-2xl
-             font-semibold text-[#244D3F]">
+              <h1 className="text-2xl font-semibold text-[#244D3F]">
                 {expectedId.goal}
               </h1>
               <p className="text-[#64748B]">Goal(days)</p>
             </div>
-            <div className="px-5 py-7 shadow bg-white rounded-md text-center">
-              <h1
-                className="text-2xl
-             font-semibold text-[#244D3F]">
+
+            <div className="px-5 py-7 shadow bg-white rounded-md text-center sm:col-span-2 md:col-span-1">
+              <h1 className="text-2xl font-semibold text-[#244D3F]">
                 {formatted}
               </h1>
               <p className="text-[#64748B]">Next Due</p>
             </div>
           </div>
+
+          {/* right section card relationship status */}
           <div className="bg-white shadow-xl p-6 rounded-md">
-            <div className="flex justify-between items-center">
+            <div className="flex  sm:flex-row justify-between items-start sm:items-center gap-3">
               <p className="text-2xl font-semibold text-[#244D3F]">
                 Relationship Goal
               </p>
               <button className="btn">Edit</button>
             </div>
+
             <p className="text-gray-500 font-semibold">
               Connect Every{' '}
               <span className="text-black">
@@ -158,11 +166,15 @@ const DetailPage = () => {
               .
             </p>
           </div>
+
+          {/* quick check in section */}
           <div className="bg-white shadow-xl p-8 rounded-md">
             <p className="text-2xl font-semibold text-[#244D3F]">
               Quick Check-In
             </p>
-            <div className=" grid grid-cols-3 gap-6 mt-6">
+
+            {/* buttons for contacts */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
               <div
                 onClick={() => handleFriendsData('Call')}
                 className="btn border-none flex items-center justify-center bg-[#f8fafc] shadow-xl p-6 rounded-md h-32">
@@ -171,6 +183,7 @@ const DetailPage = () => {
                   Call
                 </p>
               </div>
+
               <div
                 onClick={() => handleFriendsData('Text')}
                 className="btn border-none flex items-center justify-center bg-[#f8fafc] shadow-xl p-6 rounded-md h-32">
@@ -179,6 +192,7 @@ const DetailPage = () => {
                   Text
                 </p>
               </div>
+
               <div
                 onClick={() => handleFriendsData('Video')}
                 className="btn border-none flex items-center justify-center bg-[#f8fafc] shadow-xl p-6 rounded-md h-32">
