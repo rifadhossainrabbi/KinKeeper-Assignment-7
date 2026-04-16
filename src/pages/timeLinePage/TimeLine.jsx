1bc55e73cb6b7e89b625a6efaf4b03a38be2567a
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-04-16 19:38:49 +0600

    filter sections defaultValue change

--- src/pages/timeLinePage/TimeLine.jsx
+++ src/pages/timeLinePage/TimeLine.jsx
@@ -46,7 +46,7 @@ const TimeLine = () => {
         <h1 className="text-3xl font-semibold mb-3">Timeline</h1>
 
         <select
-          defaultValue="Pick a color"
+          defaultValue=""
           onChange={handleFilterOnChange}
           className="select mb-4">
           <option disabled={true}>Filter Timeline</option>
